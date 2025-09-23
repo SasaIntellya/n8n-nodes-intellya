8cdbe5ac06744a87a344d6d28fff6d35200bd987
Saša Glogovac <sasa.glogovac@intellya.ai>
2025-09-23 14:46:54 +0200

    ok

--- nodes/MarkdownToChunks/MarkdownToChunks.node.ts
+++ nodes/MarkdownToChunks/MarkdownToChunks.node.ts
@@ -1,6 +1,6 @@
 
 import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-// import { MarkdownParserService } from "./MarkdownToChunks";
+import { MarkdownParserService } from "./MarkdownToChunks";
 
 export class MarkdownToChunks implements INodeType {
     description: INodeTypeDescription = {
@@ -33,15 +33,30 @@ export class MarkdownToChunks implements INodeType {
 
         // EXECUTE
 
-
-
-        const outputData = this.getInputData();
-
-        console.log(outputData);
-    
-
-
+        const parser = new MarkdownParserService();
         const returnData: INodeExecutionData[] = [];
+        var items = this.getInputData();
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            if (item.json) {
+                var content = item.json['content']?.toString();
+                console.log(i);
+                
+                console.log(content);
+                
+                const chunks = await parser.process(content ?? '', i + "example.md");
+                console.log(chunks);
+                
+                for await (const c of chunks) {
+                    const returnItem = {
+                        json: { chunk: c }
+                    };
+                    returnData.push(returnItem);
+                }
+            }
+        }
+        console.log(returnData);
+        
         return [returnData];
     }
 }
