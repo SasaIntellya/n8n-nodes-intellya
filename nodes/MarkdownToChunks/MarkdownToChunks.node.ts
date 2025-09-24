8d1d2bbdb32224032b7215c21788204625fa377c
Saša Glogovac <sasa.glogovac@intellya.ai>
2025-09-24 13:58:57 +0200

    pagenum

--- nodes/MarkdownToChunks/MarkdownToChunks.node.ts
+++ nodes/MarkdownToChunks/MarkdownToChunks.node.ts
@@ -19,34 +19,23 @@ export class MarkdownToChunks implements INodeType {
     };
 
     async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-        // // PRIVATE METHODS
-        // const parser = new MarkdownParserService();
-
-        // const chunks = await parser.process("./example.md", "example.md");
-
-        // // const chunks = await parser.process(
-        // //     "./example.md",              // putanja do markdown fajla
-        // //     "example.md"                 // ime fajla
-        // // );
-
-        // console.log("Rezultat parsiranja:\n", JSON.stringify(chunks, null, 2));
-
         // EXECUTE
 
         const parser = new MarkdownParserService();
         const returnData: INodeExecutionData[] = [];
-        var items = this.getInputData();
+        let items = this.getInputData();
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
             if (item.json) {
-                var content = item.json['content']?.toString();
+                // var content = '<!--PAGE:'+ i + 1 + '-->' + item.json['content']?.toString();
+                let content = `<!--PAGE:${i + 1}-->${item.json['content']?.toString()}`;
                 console.log(i);
-                
+
                 console.log(content);
-                
+
                 const chunks = await parser.process(content ?? '', i + "example.md");
                 console.log(chunks);
-                
+
                 for await (const c of chunks) {
                     const returnItem = {
                         json: { chunk: c }
@@ -56,7 +45,7 @@ export class MarkdownToChunks implements INodeType {
             }
         }
         console.log(returnData);
-        
+
         return [returnData];
     }
 }
