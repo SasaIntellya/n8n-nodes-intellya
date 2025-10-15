9d928e24631660df986a160d84c52a9b4aab730e
Saša Glogovac <sasa.glogovac@intellya.ai>
2025-10-15 10:26:32 +0200

    ok

--- nodes/VLM/Model/OpenAI.ts
+++ nodes/VLM/Model/OpenAI.ts
@@ -2,13 +2,13 @@ export class OpenAIRequest {
     model: string;
     max_completion_tokens: number;
     messages: OpenAIMessage[];
-    response_format: string;
+    response_format: {};
 
     constructor(model: string, max_completion_tokens: number, messages: OpenAIMessage[], response_format = 'text') {
         this.model = model;
         this.max_completion_tokens = max_completion_tokens;
         this.messages = messages;
-        this.response_format = response_format;
+        this.response_format = { type: response_format };
     }
 }
 
