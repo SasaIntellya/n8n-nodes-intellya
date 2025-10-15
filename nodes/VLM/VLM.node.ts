66e100eaa3c99500dcad5ff317b6fc5874015448
Saša Glogovac <sasa.glogovac@intellya.ai>
2025-10-15 12:10:26 +0200

    ok

--- nodes/VLM/VLM.node.ts
+++ nodes/VLM/VLM.node.ts
@@ -6,7 +6,7 @@ export class VLM implements INodeType {
     description: INodeTypeDescription = {
         displayName: 'VLM',
         name: 'VLM',
-        icon: 'file:sasa.svg',
+        icon: 'file:VLM.svg',
         group: ['transform'],
         version: 1,
         description: 'VLM',
