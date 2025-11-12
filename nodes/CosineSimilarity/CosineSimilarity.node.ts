0a253926cd9b28fd1a98eb793692f3e54fdfb834
Saša Glogovac <sasa.glogovac@intellya.ai>
2025-11-12 18:04:27 +0100

    ok

--- nodes/CosineSimilarity/CosineSimilarity.node.ts
+++ nodes/CosineSimilarity/CosineSimilarity.node.ts
@@ -76,7 +76,7 @@ export class CosineSimilarity implements INodeType {
         let elasticCosineSimilarity = async (): Promise<any> => {
             let url = `https://${this.getNodeParameter('username', 0) as string}:${this.getNodeParameter('password', 0) as string}@${this.getNodeParameter('baseUrl', 0) as string}/${this.getNodeParameter('index', 0) as string}/_search`;
             var body = {
-                size: 10,
+                size: this.getNodeParameter('username', 0) as number,
                 query: {
                     bool: {
                         filter: [
